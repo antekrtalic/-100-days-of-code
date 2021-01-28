8e16e1f34aa5841c06fb24c8a3f113a0904ec11d
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-01-28 21:35:47 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,31 @@
-print("Hello Python world!")
+#LIST METHODS
+
+#.append()
+#.insert()
+#del
+#.pop()
+#.remove()
+#.sort()
+#.sorted()
+#.reverse()
+#len()
+
+
+languages = ["English", "Spanish", "Japanese", "Russian", "Korean"]
+languages.append("Portugese")
+print(languages)
+languages.insert(1,"Croatian")
+print(languages)
+del languages[1]
+print(languages)
+languages.pop()
+print(languages)
+languages.remove("Spanish")
+print(languages)
+languages.sort()
+print(languages)
+sorted(languages)
+print(languages)
+languages.reverse()
+print(languages)
+print(len(languages))
