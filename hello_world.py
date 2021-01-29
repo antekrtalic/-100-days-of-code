b28046b1e64ad42f1d56eaf258ec5038971d7991
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-01-29 11:33:57 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,31 +1,6 @@
-#LIST METHODS
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+	print(f"{magician.title()}, that was a great trick.")
+	print(f"I can't wait to see your next trick, {magician.title()}.\n")
 
-#.append()
-#.insert()
-#del
-#.pop()
-#.remove()
-#.sort()
-#.sorted()
-#.reverse()
-#len()
-
-
-languages = ["English", "Spanish", "Japanese", "Russian", "Korean"]
-languages.append("Portugese")
-print(languages)
-languages.insert(1,"Croatian")
-print(languages)
-del languages[1]
-print(languages)
-languages.pop()
-print(languages)
-languages.remove("Spanish")
-print(languages)
-languages.sort()
-print(languages)
-sorted(languages)
-print(languages)
-languages.reverse()
-print(languages)
-print(len(languages))
+print("Thank you everyone, that was a great show.")
