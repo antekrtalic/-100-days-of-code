d31cba41d38ff6427a23eb743cb3e95d357e697e
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-01-29 15:12:41 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,2 @@
-pizzas = ["neapolitan", "chicago", "new-york"]
-for pizza in pizzas:
-	print(pizza)
-	print(f"{pizza.title()} is the best pizza ever.")
-
-print("I just enjoy eating pizza!")
-print("Pizza has great taste.")
-print("I like it creespy, with a lot of cheese, and thin.")
-print("I really love pizza.")
+cubes = [cube**3 for cube in range(1,11)]
+print(cubes)
