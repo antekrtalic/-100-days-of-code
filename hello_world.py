5a65fd247026ea564865800e29a59f1b66f5d148
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-01 13:55:44 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,5 @@
 players = ["charles", "martina", "michael", "florence", "eli"]
-print(players[0:3])
+
+print("Here are first three players on my team:")
+for player in players[:3]:
+	print(player.title())
