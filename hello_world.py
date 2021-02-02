234667d2d56cbf53aee9428128d789ab22d3f81d
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-02 13:27:18 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,12 @@
-age = 17
-if age >= 18:
-	print("You are old enough to vote!")
-	print("Have you registered to vote yet?")
-else:
-	print("Sorry, you are too young to vote.")
-	print("Please register to vote as soon as you turn 18!")
+age = 12
+
+if age < 4:
+	price = 0
+elif age < 18:
+	price = 25
+elif age < 65:
+	price = 40
+elif age >= 65:
+	price = 20
+
+print(f"Your admission cost is ${price}.")
