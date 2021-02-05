cdcb7a080c94ffa81165d8d3964451874efd2a50
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-05 13:26:05 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,9 @@
-user_0 = {
-	'username': 'efermi',
-	'first': 'enrico',
-	'last': 'fermi',
+favorite_languages = {
+	'jen': 'python',
+	'sarah': 'c',
+	'edward': 'ruby',
+	'phil': 'python',
 }
 
-for key,value in user_0.items():
-	print(f"\nKey: {key}")
-	print(f"Value: {value}")
+for name,language in favorite_languages.items():
+	print(f"{name.title()}'s favorite language is {language.title()}.")
