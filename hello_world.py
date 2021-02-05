e1fc6ccf487f00fd5de521384496deb4e1dd64e6
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-05 13:32:12 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -5,5 +5,10 @@ favorite_languages = {
 	'phil': 'python',
 }
 
-for name,language in favorite_languages.items():
-	print(f"{name.title()}'s favorite language is {language.title()}.")
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+	print(name.title())
+
+	if name in friends:
+		language = favorite_languages[name].title()
+		print(f"\t{name.title()}, I see you love {language}!")
