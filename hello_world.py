280d733cca6ea7a53dd908caca40e4063052c813
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-05 13:17:31 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,17 +1,9 @@
-alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
-print(f"Original x-position: {alien_0['x_position']}")
+user_0 = {
+	'username': 'efermi',
+	'first': 'enrico',
+	'last': 'fermi',
+}
 
-#Move the alien to the right
-#Determine how far to move alien based on its current speed
-if alien_0['speed'] == "slow":
-	x_increment = 1
-elif alien_0['speed'] == "medium":
-	x_increment = 2
-else:
-	#This must be fast alien
-	x_increment = 3
-
-#The new position is the old position plus the increment.
-alien_0['x_position'] = alien_0['x_position'] + x_increment
-
-print(f"New x-position: {alien_0['x_position']}")
+for key,value in user_0.items():
+	print(f"\nKey: {key}")
+	print(f"Value: {value}")
