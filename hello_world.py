73f4dcb064915a5d2e7fa63f34e19c2a2c68cd5a
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-05 11:06:08 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,17 @@
-alien_0 = {"color": "green", "points": 5}
-new_points = alien_0["points"]
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+print(f"Original x-position: {alien_0['x_position']}")
 
-print(f"You just earned {new_points} points!")
+#Move the alien to the right
+#Determine how far to move alien based on its current speed
+if alien_0['speed'] == "slow":
+	x_increment = 1
+elif alien_0['speed'] == "medium":
+	x_increment = 2
+else:
+	#This must be fast alien
+	x_increment = 3
+
+#The new position is the old position plus the increment.
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+
+print(f"New x-position: {alien_0['x_position']}")
