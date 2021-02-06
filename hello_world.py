e1024e1f66706ae1bdf4edd7398b3d39131ab5ad
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-06 22:02:56 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,22 @@
-favorite_numbers = {
-	'ante': {
-		'numbers': [2,5,13,20],
+cities = {
+	'rio de janeiro': {
+		'country': 'brazil',
+		'population': '6.5 million',
+		'fact': 'Rio de Janeiro means January River, but the river is actually a bay',
 	},
-	'lucy': {
-		'numbers': [32,18,4,5],
+	'moscow': {
+		'country': 'russia',
+		'population': '12.7 million',
+		'fact': 'Moscow claims the largest number of billionaires in the world. Per Forbes, there are 84 billionaires in the city',
 	},
-	'carla': {
-		'numbers': [0,11,55,28],
+	'tokyo': {
+		'country': 'japan',
+		'population': '9.2 million',
+		'fact': ' Tokyo is the largest metropolitan in the world, hosting over 36 million people spread over 3 prefectures.',
 	},
 }
 
-for name, number in favorite_numbers.items():
-	print(f"\nName: {name.title()}")
-	print(f"\n\tYour favorite numbers are: {number['numbers']}")
+for city,city_info in cities.items():
+	print(f"\nName of the city: {city.title()}")
+	print(f"\n\t{city.title()} is city places in {city_info['country'].title()} with population {city_info['population']}.")
+	print(f"\n\t{city_info['fact']}.")
