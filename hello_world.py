69b8e30cb8fe542982f3d4085daa932ba68306b3
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-06 21:23:58 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,20 @@
-favorite_languages = {
-	'jen': ['python', 'ruby'],
-	'sarah': ['c'],
-	'edward': ['ruby','go'],
-	'phil': ['python', 'haskell'],
+users = {
+	'aeinstein': {
+		'first': 'albert',
+		'last': 'einstein',
+		'location': 'princeton',
+	},
+	'mcurie': {
+		'first': 'marie',
+		'last': 'curie',
+		'location': 'paris',
+	},
 }
 
-for name,languages in favorite_languages.items():
-	print(f"\n{name.title()}'s favorite languages are:")
-	for language in languages:
-		print(f"\t{language.title()}")
+for username, user_info in users.items():
+	print(f"\nUsername: {username}")
+	full_name = f"{user_info['first']} {user_info['last']}"
+	location = user_info['location']
+
+	print(f"\tFull name: {full_name.title()}")
+	print(f"\tLocation: {location.title()}")
