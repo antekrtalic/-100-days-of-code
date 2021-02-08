885d5f4a002688a7eb0d05479a15ae9c502f5623
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-08 10:43:40 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,15 @@
-def describe_pet(animal_type, pet_name):
-    """Display information about pet."""
-    print(f"\nI have a {animal_type}.")
-    print(f"My {animal_type}'s name is {pet_name.title()}.")
-describe_pet("hamster", "harry")
-describe_pet("dog", "willie")
+def get_formatted_name(first_name, last_name):
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+while True:
+    print("\nPlease tell me your name:")
+    f_name = input("First name: ")
+    if f_name == "quit":
+        break
+    l_name = input("Last name: ")
+    if l_name == "quit":
+        break
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
