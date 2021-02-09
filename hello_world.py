1628bac54be522b245fe95655fb396fd1b85bd45
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-09 11:41:35 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -17,8 +17,26 @@ class Car:
         """Print a statement showing the car's mileage."""
         print(f"This car has {self.odometer_reading} miles on it.")
 
-my_new_car = Car('audi', 'a4', 2019)
-print(my_new_car.get_descriptive_name())
+    def update_odometer(self, mileage):
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
 
-my_new_car.odometer_reading = 23
-my_new_car.read_odometer()
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading. """
+        self.odometer_reading += miles
+
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
