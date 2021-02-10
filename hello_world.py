7638665f2d78960c04b73bbfff525e1ce0760b2f
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-10 16:46:24 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,58 +1,22 @@
-class Car:
-    """A simple attempt to represent a car."""
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
+from random import choice
+from random import choices
 
-    def get_descriptive_name(self):
-        long_name = f"{self.year} {self.make} {self.model}"
-        return long_name.title()
+lottery = ["M", "A", 3, 11, 15, 26, 18, 19, 4, 2, 55, 63, "R", "L"]
+prize_combination = []
 
-    def read_odometer(self):
-        print(f"This car has {self.odometer_reading} miles on it.")
+while len(prize_combination) != 4:
+    current = choice(lottery)
+    prize_combination.append(current)
 
-    def update_odometer(self, mileage):
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
+print(f"This is prize combination: {prize_combination}")
 
-    def increment_odometer(self, miles):
-        self.odometer_reading += miles
+my_ticket = []
+count = 0
 
-class Battery:
-    """A simple attempt to model a battery for an electric car."""
+while my_ticket != prize_combination:
+    my_ticket = choices(lottery, k=4)
+    count += 1
 
-    def __init__(self, battery_size=75):
-        self.battery_size = battery_size
-
-    def describe_battery(self):
-        """Print a statement describing the battery size."""
-        print(f"This car has a {self.battery_size}-kwH battery.")
-
-    def get_range(self):
-        """Print a statement about the range this battery provides."""
-        if self.battery_size == 75:
-            range = 260
-        elif self.battery_size == 100:
-            range = 315
-
-        print(f"This car can go about {range} miles on full charge.")
-
-class ElectricCar(Car):
-    """Represent aspect of a car, specific to eletric vehicle."""
-
-    def __init__(self, make, model, year):
-        """
-        Initialize attributes of the parent class.
-        Then initialize attributes specific to an electric car.
-        """
-        super().__init__(make, model, year)
-        self.battery = Battery()
-
-my_tesla = ElectricCar('tesla', 'model s', 2019)
-print(my_tesla.get_descriptive_name())
-my_tesla.battery.describe_battery()
-my_tesla.battery.get_range()
+if my_ticket == prize_combination:
+    print(f"You win first prize! It took you {count} guesses:")
+    print(my_ticket, prize_combination)
