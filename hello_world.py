032ace842406e94e9242497cb46d0a877c32969f
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-13 23:47:31 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,24 @@
-def list_manipulation(arry, command, location, value):
-    if command == "add" and location == "beginning":
-        return [value] + arry
+''
+def isEven(num):
+    return num % 2 == 0
 
-print(list_manipulation([1,2,3], "add", "beginning", 20))
+partition([1,2,3,4], isEven) # [[2,4],[1,3]]
+'''
+def isEven(brojevi):
+    return brojevi % 2
+
+
+    
+def partition(lista, func):
+    both = []
+    even = []
+    odd = []
+    for num in lista:
+        x = func(num)
+        if x == True:
+            even.append(num)
+        else:
+            odd.append(num)
+    both.append(even)
+    both.append(odd)
+    return both
