2bc1f4ab29f19e45e561cae900e6e61e81bea483
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-16 21:24:35 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,24 +1,4 @@
-''
-def isEven(num):
-    return num % 2 == 0
+def sum_floats(*fls):
+    return sum(fl for fl in fls if type(fl) == float)
 
-partition([1,2,3,4], isEven) # [[2,4],[1,3]]
-'''
-def isEven(brojevi):
-    return brojevi % 2
-
-
-    
-def partition(lista, func):
-    both = []
-    even = []
-    odd = []
-    for num in lista:
-        x = func(num)
-        if x == True:
-            even.append(num)
-        else:
-            odd.append(num)
-    both.append(even)
-    both.append(odd)
-    return both
+print(sum_floats(1.5, 2.4, 'awesome', [], 1))
