91fcc0fb1b1bccfe93107ac616fae3d1009300db
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-17 20:47:41 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,11 @@
-def extract_full_name(arry):
-    return [" ".join(i.values()) for i in arry]
-    
-print(names = [{'first': 'Elie', 'last': 'Schoppik'}, {'first': 'Colt', 'last': 'Steele'}])
+def divide(a, b):
+    try:
+       result = a/b
+    except (ZeroDivisionError, TypeError) as err:
+        print("Something went wrong!")
+        print(err)
+    else:
+        print(f"{a} divided by {b} is {result}")
 
+print(divide(1,2))
+print(divide(1,"a"))
