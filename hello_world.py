80c600a0571d2ecfddb89f6f66fbe5e93034c645
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-18 09:50:09 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,18 @@
-def add_numbers(a, b, c, d):
-    import pdb; pdb.set_trace()
+from pyfiglet import figlet_format
+from termcolor import colored
 
-    return a + b + c + d
 
-add_numbers(1,2,3,4)
+
+def print_art(msg,color):
+    valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan")
+
+    if color not in valid_colors:
+        color = "magenta"
+        
+    result = figlet_format(msg)
+    result_color = colored(result, color=color)
+    print(result_color)
+
+msg = input("What would you like to print?")
+color = input("What color?")
+print_art(msg,color)
