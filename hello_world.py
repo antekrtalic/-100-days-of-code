2b94588bfdcab3ef1ab43356aa67695b4b29f1e9
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-19 13:15:02 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -4,8 +4,24 @@ class User:
         self.last = last
         self.age = age
 
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
+    def initials(self):
+        return f"{self.first[0]}.{self.last[0]}."
+
+    def likes(self, thing):
+        return f"{self.first} likes {thing}"
+
+    def is_senior(self):
+        return self.age >= 65
+
+    def birthday(self):
+        self.age += 1
+        return f"Happy {self.age}th, {self.first}"
+
 
 user1 = User("Joe", "Smith", 68)
 user2 = User("Blanca", "Lopez", 41)
-print(user1.first, user1.last)
-print(user2.first, user2.last)
+print(user1.birthday())
+print(user2.is_senior())
