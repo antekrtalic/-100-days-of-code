79fd691b9bcf5a4da73125e6faad0377681ac747
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-19 10:36:19 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,11 @@
-from pyfiglet import figlet_format
-from termcolor import colored
+class User:
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
 
 
-
-def print_art(msg,color):
-    valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan")
-
-    if color not in valid_colors:
-        color = "magenta"
-        
-    result = figlet_format(msg)
-    result_color = colored(result, color=color)
-    print(result_color)
-
-msg = input("What would you like to print?")
-color = input("What color?")
-print_art(msg,color)
+user1 = User("Joe", "Smith", 68)
+user2 = User("Blanca", "Lopez", 41)
+print(user1.first, user1.last)
+print(user2.first, user2.last)
