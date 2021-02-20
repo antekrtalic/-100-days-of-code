2c5f318622cebd9e44d404f59e0a9019a0551ca0
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-20 22:06:58 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,6 @@
 filename = 'pi_digits.txt'
 with open(filename) as file_object:
-    for line in file_object:
-        print(line.rstrip())
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
