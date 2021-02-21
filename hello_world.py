ffb29a7fd83799d538a52d5ba4e31dc66a385259
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-21 19:28:15 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,24 +1,6 @@
-filename = "cat.txt"
-dog_file = "dog.txt"
+import json
 
-try:
-    with open(filename, "a") as file_object:
-        for x in range(3):
-            user = input("Enter cat's name ")
-            file_object.write(f"{user}\n")
-
-    with open(filename) as file_objectt:
-        for line in file_objectt:
-            print(line.strip())
-
-    with open(dog_file, "a") as dog_object:
-        for x in range(3):
-            user = input("Enter dog's name ")
-            dog_object.write(f"{user}\n")
-
-    with open(dog_file) as dog_object:
-        for dog in dog_object:
-            print(dog.strip())
-
-except FileNotFoundError:
-    print("File not found!")
+filename = "username.json"
+with open(filename) as f:
+    username = json.load(f)
+    print(f"Welcome back, {username}!")
