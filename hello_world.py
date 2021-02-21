2fdc0a0a1e13b606231c25190e54d0c71ee4d9e0
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-21 10:50:38 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,15 +1,17 @@
 def count_words(filename):
     """Count the approximate number of words in a file."""
-    try:
-        with open(filename, encoding="utf-8") as f:
-            contents = f.read()
-    except FileNotFoundError:
-        print(f"Sorry, the file {filename} does not exist.")
-    else:
-        # Count the approximate number of words in file.
-        words = contents.split()
-        num_words = len(words)
-        print(f"The file {filename} has about {num_words} words.")
+    filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt', 'little_woman.txt']
+    for filename in filenames:
+        try:
+            with open(filename, encoding="utf-8") as f:
+                contents = f.read()
+        except FileNotFoundError:
+            pass
+        else:
+            # Count the approximate number of words in file.
+            words = contents.split()
+            num_words = len(words)
+            print(f"The file {filename} has about {num_words} words.")
 
 filename = "alice.txt"
 count_words((filename))
