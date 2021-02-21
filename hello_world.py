10969761279d78a27289c9cb3333915d390db1d4
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-21 10:15:09 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,8 @@
-filename = "guest.txt"
+filename = "responses.txt"
 
 with open(filename, "a") as file_object:
     x = 0
     while x in range(3):
-        user = input("What is your name? ")
-        print(f"Welcome {user}")
+        user = input("Why do you like programming so much? ")
         file_object.write(f"{user}\n")
-        print(f"You were successfully added in the {filename}")
         x += 1
