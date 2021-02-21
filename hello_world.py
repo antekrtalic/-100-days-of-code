1ae380a43054e883aa7c540121a2ed6df74832ff
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-21 10:45:40 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,16 +1,15 @@
-print("Give me two numbers and I'll divide them.")
-print("Enter 'q' to quit.")
-
-while True:
-    first_number = input("\nFirst number: ")
-    if first_number == 'q':
-        break
-    second_number = input("Second number: ")
-    if second_number == 'q':
-        break
+def count_words(filename):
+    """Count the approximate number of words in a file."""
     try:
-        answer = int(first_number) / int(second_number)
-    except ZeroDivisionError:
-        print("You can't divide by 0!")
+        with open(filename, encoding="utf-8") as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} does not exist.")
     else:
-        print(answer)
+        # Count the approximate number of words in file.
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {filename} has about {num_words} words.")
+
+filename = "alice.txt"
+count_words((filename))
