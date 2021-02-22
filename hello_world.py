33195f56bd6866a43421b1208921573dab5d35a2
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-02-22 10:54:26 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,29 +1,38 @@
 import json
 
-def get_stored_username():
-    """Get stored username if available."""
-    filename = "username.json"
+def add_num():
+    """Storing favorite num inside json file"""
+    filename = "favorite_number.json"
+    user_num = input("What is your favorite num?\n ")
+    
+    with open(filename, 'w') as f:
+        json.dump(user_num, f)
+
+
+def read_num():
+    """Reading favorite number"""
+    filename = "favorite_number.json"
     try:
         with open(filename) as f:
-            username = json.load(f)
+            number = json.load(f)
     except FileNotFoundError:
         return None
     else:
-        return username
+        return f"I know your favorite number! It's {number}."
 
-def get_new_username():
-    """Prompt for a new username."""
-    username = input("What is your name? ")
-    filename = 'username.json'
-    with open(filename,'w') as f:
-        json.dump(username, f)
-    return username
+def new_num():
+    """Adding new number"""
+    filename = 'favorite_number.json'
+    user_num = input("What number do you want to add?\n ")
+    with open(filename) as f:
+        read = json.load(f)
 
-def greet_user():
-    """Greet the user by name."""
-    username = get_stored_username()
-    if username:
-        print(f"Welcome back, {username}!")
+    if user_num in read:
+        return f"That number is already in {filename}"
     else:
-            username = get_new_username()
-            print(f"We'll remember you when you come back, {username}!")
+        with open(filename, "a") as f:
+            json.dump(user_num, f)
+            return f"We store your number {user_num}"
+add_num()
+print(read_num())
+print(new_num())
