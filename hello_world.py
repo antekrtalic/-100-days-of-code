07f1540d1cff60a75288292b4e5dda0c558eef57
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-04 10:07:34 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,36 +1,61 @@
-import sys
+class User:
+    active_users = 0
 
-import pygame
+    @classmethod
+    def display_active_users(cls):
+        return f"There are currently {cls.active_users} active users"
 
-from wooden_boy import Wooden_Boy
+    @classmethod
+    def from_string(cls, data_str):
+        first, last, age = data_str.split(",")
+        return cls(first, last, int(age))
 
-class Window:
+    def __init__(self, first, last, age):
+        self.first = first
+        self.last = last
+        self.age = age
+        User.active_users += 1
+
+    def logout(self):
+        User.active_users -= 1
+        return f"{self.first} has logged out"
+
+    def full_name(self):
+        return f"{self.first} {self.last}"
+
+    def initials(self):
+        return f"{self.first[0]}.{self.last[0]}."
+
+    def likes(self, thing):
+        return f"{self.first} likes {thing}"
+
+    def is_senior(self):
+        return self.age >= 65
+
+    def birthday(self):
+        self.age += 1
+        return f"Happy {self.age}th, {self.first}"
 
 
-    def __init__(self):
+class Moderator(User):
+    total_mods = 0
+    def __init__(self, first, last, age, community):
+        super().__init__(first, last, age)
+        self.community = community
+        Moderator.total_mods += 1
 
-        pygame.init()
+    @classmethod
+    def display_active_mods(cls):
+        return f"There are currently {cls.total_mods} active mods"
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Pygame Window")
+    def remove_post(self):
+        return f"{self.full_name()} remove a post from the {self.community} community"
 
-        self.boy = Wooden_Boy(self)
+u1 = User("Tom", "Garcia", 35)
+u2 = User("Tom", "Garcia", 35)
+u3 = User("Tom", "Garcia", 35)
+jasmine = Moderator("Jasmine", "O'Conner", 61, "Piano")
+jasmine2 = Moderator("Jasmine", "O'Conner", 61, "Piano")
 
-        self.bg_color = (0, 0, 255)
-
-
-    def run_window(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-
-            self.screen.fill(self.bg_color)
-            self.boy.blitme()
-
-            pygame.display.flip()
-
-
-if __name__ == "__main__":
-    win = Window()
-    win.run_window()
+print(User.display_active_users())
+print(Moderator.display_active_mods())
