fc8f55189ee27a67dda11b406d180c9a31253b14
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-11 09:41:40 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,35 +1,10 @@
-from copy import copy
-
-class Human:
-    def __init__(self, first, last, age):
-        self.first = first
-        self.last = last
-        self.age = age
-
-    def __repr__(self):
-        return f"Human named {self.first} {self.last} aged {self.age}"
-
-    def __len__(self):
-        return self.age
-
-    def __add__(self, other):
-        if isinstance(other, Human):
-            return Human(first="Newborn", last=self.last, age=0)
-        return "You can't add that!"
-
-    def __mul__(self, other):
-        if isinstance(other, int):
-            return [copy(self) for i in range(other)]
-        return "Can't multiply"
-
-j = Human("Jenny", "Larsen", 47)
-k = Human("Kevin", "Jones", 49)
+def get_unlimited_multiples(number=1):
+  sum = number
+  while True:
+    yield sum
+    sum += number
 
 
-triples = j * 3
-triples[1].first = "Jessica"
-print(triples)
 
-# kevin and jessica having triplets
-triplets = (k + j) * 3
-print(triplets)
+sevens = get_unlimited_multiples(7)
+[print(next(sevens)) for i in range(15)]
