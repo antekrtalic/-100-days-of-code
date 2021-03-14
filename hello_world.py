8258962450a8e47073dfc74e859b629a091667e9
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-14 11:22:56 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,28 +1,16 @@
-from time import time
 from functools import wraps
 
-def speed_test(fn):
+def show_args(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
-        start_time = time()
-        result = fn(*args, **kwargs)
-        end_time = time()
-        print(f"Executing {fn.__name__}")
-        print(f"Time Elapsed: {end_time-start_time}")
-        return result
+        print("Here are the args: ", args)
+        print("Here are the kwargs: ", kwargs)
+        return fn(*args, **kwargs)
     return wrapper
 
+@show_args
+def do_nothing(*args, **kwargs):
+    pass
 
 
-
-
-@speed_test
-def sum_nums_gen():
-    return sum(x for x in range(90000000))
-
-@speed_test
-def sum_nums_list():
-    return sum([x for x in range(90000000)])
-
-print(sum_nums_gen())
-print(sum_nums_list())
+do_nothing(1, 2, 3,a="hi",b="bye")
