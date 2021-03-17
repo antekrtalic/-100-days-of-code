4b944f69615917222374a1587801fc7683535bde
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-17 19:07:17 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,47 +1,14 @@
-# def add(a, b):
-#    """
-#     >>> add(2, 3)
-#    5
-#    >>> add(100, 200)
-#    300
-#    """
-#    return a + b
+fname = input("Enter file name: ")
+try:
+    fhand = open(fname)
+except:
+    print("File cannot be opened", fname)
+    exit()
 
-def double(values):
-    """ double the values in a list
-    >>> double([1,2,3,4])
-    [2, 4, 6, 8]
+count = 0
+for line in fhand:
+    line = line.rstrip()
+    if line.startswith("Subject:"):
+        count = count + 1
+    print('There were', count, 'subject lines in', fname)
 
-    >>> double([])
-    []
-
-    >>> double(['a', 'b', 'c'])
-    ['aa', 'bb', 'cc']
-
-    >>> double([True, None])
-    Traceback (most recent call last):
-        ...
-    TypeError: unsupported operand type(s) for *: 'int' and 'NoneType'
-    """
-    return [2 * element for element in values]
-
-def say_hi():
-    """
-    >>> say_hi()
-    'hi'
-    """
-    return "hi"
-
-def true_that():
-    """
-    >>> true_that()
-    True
-    """
-    return True
-
-def make_dict():
-    """
-    >>> make_dict(['a', 'b'])
-    {'a': True, 'b': True}
-    """
-    return {key: True for key in keys}
