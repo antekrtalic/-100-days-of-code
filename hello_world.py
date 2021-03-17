1e669cfaaf3bc1856315c48c8fe75b0eaea60867
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-17 22:31:22 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,15 @@
-fname = input("Enter file name: ")
-try:
-    fhand = open(fname)
-except:
-    print("File cannot be opened", fname)
-    exit()
-
+file_name = input("Enter file name: ")
+if file_name == "na na boo boo":
+    print("NA NA BOO BOO TO YOU TOO - You have been punk'd!")
+fhand = open(file_name)
+sum = 0
 count = 0
-for line in fhand:
-    line = line.rstrip()
-    if line.startswith("Subject:"):
-        count = count + 1
-    print('There were', count, 'subject lines in', fname)
+total_count = 0
+for sent in fhand:
+    if sent.startswith("X-DSPAM-Confidence:"):
+        x = sent.find(":")
+        sum += float(sent[x+1:].strip())
+        count += 1
 
+total_count = sum / count
+print("Average spam confidence:", total_count)
