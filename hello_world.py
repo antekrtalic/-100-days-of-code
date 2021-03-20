c57e79a814da66f03cb467f76cce8db7d542fd4d
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-20 21:12:18 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,14 @@
 import re
-hand = open('mbox-short.txt')
+hand = open('mbox.txt')
+count = 0
+sum = 0
+
 
 for line in hand:
     line = line.rstrip()
-    x = re.findall('^From .* ([0-9][0-9]):', line)
-    if len(x) > 0: print(x)
+    x = re.findall('^Details:.*rev=([0-9]+)', line)
+    if len(x) > 0:
+        count += 1
+        sum += int(x[0])
+
+print(int(sum / count))
