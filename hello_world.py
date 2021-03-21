01169f1d2068b800098f8e4964b7cefae189613b
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-21 11:54:20 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,14 @@
-import urllib.request, urllib.parse, urllib.error
+import socket
 
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org', 80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+mysock.send(cmd)
 
-counts = dict()
-for line in fhand:
-    words = line.decode().split()
-    for word in words:
-        counts[word] = counts.get(word, 0) + 1
+while True:
+    data = mysock.recv(512)
+    if len(data) < 1:
+        break
+    print(data.decode(), end="")
 
-print(counts)
+mysock.close()
