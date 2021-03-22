4d2b791a8c994424da40100090c3292b094b677c
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-22 20:44:19 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,13 +1,22 @@
 import xml.etree.ElementTree as ET
 
-data = '''<person>
-    <name>Chuck</name>
-    <phone type="int1">
-        +1 734 303 4456
-    </phone>
-    <email hide="yes"/>
-</person>'''
+input = '''<stuff>
+    <users>
+        <user x="2">
+            <id>001</id>
+            <name>Chuck</name>
+        </user>
+        <user x="7">
+            <id>009</id>
+            <name>Brent</name>
+        </user>
+    </users>
+</stuff>'''
 
-tree = ET.fromstring(data)
-print('Name:', tree.find('name').text)
-print('Attr:', tree.find('email').get('hide'))
+stuff = ET.fromstring(input)
+lst = stuff.findall('users/user')
+print('User count:', len(lst))
+for item in lst:
+    print('Name', item.find('name').text)
+    print('Id', item.find('id').text)
+    print('Attribute', item.get("x"))
