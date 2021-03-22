f280d8f7c7ebe8363277b321f21de8821e4dc0ae
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-22 21:04:24 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,22 +1,15 @@
-import xml.etree.ElementTree as ET
+import json
+data = '''{
+    "name": "Chuck",
+    "phone": {
+        "type": "intl",
+        "number": "+1 734 303 4456"
+    },
+    "email": {
+        "hide": "yes"
+    }
+}'''
 
-input = '''<stuff>
-    <users>
-        <user x="2">
-            <id>001</id>
-            <name>Chuck</name>
-        </user>
-        <user x="7">
-            <id>009</id>
-            <name>Brent</name>
-        </user>
-    </users>
-</stuff>'''
-
-stuff = ET.fromstring(input)
-lst = stuff.findall('users/user')
-print('User count:', len(lst))
-for item in lst:
-    print('Name', item.find('name').text)
-    print('Id', item.find('id').text)
-    print('Attribute', item.get("x"))
+info = json.loads(data)
+print('Name', info["name"])
+print('Hide', info["email"]["hide"])
