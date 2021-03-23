d73fa66926da168e67eedcc14f3dd54c65f3fd88
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-23 22:24:46 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,22 +1,47 @@
+import urllib.request, urllib.parse, urllib.error
 import json
+import ssl
 
-data = '''[
-    {
-        "id": "001",
-        "x": "2",
-        "name": "Chuck"
-    },
-    {
-        "id": "009",
-        "x": "7",
-        "name": "Brent"
-    }
-]'''
+api_key = False
 
-info = json.loads(data)
-print('User count', len(info))
+if api_key is False:
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+else:
+    serviceurl = 'https://maps.googleapis.com/maps/api/geocode/json?'
 
-for item in info:
-    print('Name', item['name'])
-    print('Id', item['id'])
-    print('Attribut', item['x'])
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+while True:
+    address = input("Enter location: ")
+    if len(address) < 1: break
+
+    parms = dict()
+    parms['address'] = address
+    if api_key is not False: parms['key'] = api_key
+    url = serviceurl + urllib.parse.urlencode(parms)
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url, context=ctx)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not json or 'status' not in js or js['status'] != 'OK':
+        print('====Failure To Retrieve====')
+        print(data)
+        continue
+
+    print(json.dumps(js, indent=4))
+
+    lat = js['results'][0]['geometry']['location']['lat']
+    lng = js['results'][0]['geometry']['location']['lng']
+    print('lat', lat, 'lng', lng)
+    location = js['results'][0]['formatted_address']
+    print(location)
