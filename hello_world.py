ecb414baa992f8017225c97ab84d3f33c7ec3327
antekrtalic <33372386+antekrtalic@users.noreply.github.com>
2021-03-25 08:43:51 +0100

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,17 @@
 class PartyAnimal:
     x = 0
+    name = ""
+    def __init__(self, z):
+        self.name = z
+        print(self.name,"constructed")
 
     def party(self):
-        self.x = self.x + 2
-        print(self.x)
+        self.x = self.x + 1
+        print(self.name,"party count",self.x)
 
+s = PartyAnimal("Sally")
+s.party()
 
-an = PartyAnimal()
-an.party()
-an.party()
+j = PartyAnimal("Jim")
+j.party()
+s.party()
